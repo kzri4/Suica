6dac3c40fd272397c8bff75658db467145a552cb
kzri4 <kazu4126ab@gmail.com>
2021-02-24 07:26:30 +0900

    指摘された部分の修正

--- index.php
+++ index.php
@@ -2,49 +2,66 @@
 
 echo "Suicaにチャージします。金額に該当する番号を入力して下さい。" . PHP_EOL;
 
-$selects = array("1:" => 1000 ,"2:" => 3000 ,"3:" => 5000);
+$selects = ["1" => 1000 ,"2" => 3000 ,"3" => 5000];
 
-foreach ($selects as $select_num => $select_money){
-    echo $select_num . $select_money. "円" . PHP_EOL;
+foreach ($selects as $select_num => $select_money) {
+    echo $select_num . ":" . $select_money . "円" . PHP_EOL;
 }
 
 echo "番号:";
 $num = fgets(STDIN);
 $messge = "円チャージしました。" . PHP_EOL;
 
-if ($num == 1){
-    $money = $selects["1:"];
-    echo $money . $messge;
+switch ($num) {
+    case 1:
+        $money = $selects["1"];
+        echo $money . $messge;
+        break;
 
-}elseif ($num == 2){
-    $money = $selects["2:"];
-    echo $money . $messge;
+    case 2:
+        $money = $selects["2"];
+        echo $money . $messge;
+        break;
 
-}elseif ($num == 3){
-    $money = $selects["3:"];
-    echo $money . $messge;
+    case 3: 
+        $money = $selects["3"];
+        echo $money . $messge;
+        break;
 
-}else{
-    echo "無効な番号です。処理を終了します。";
+    default:
+        break;
 }
 
-echo "商品の価格を入力して下さい:";
-$price = fgets(STDIN);
+if (isset($money)) {
+    $shopping_flag = "true";
+}
 
-while ($price <= $money ){
-    $rem = ($money - $price);
-    echo "残高は{$rem}円です。" . PHP_EOL;
+if ($shopping_flag !== "true"){
+    echo "無効な番号です" . PHP_EOL . "処理を終了します。" . PHP_EOL . "買い物を終了します。" . PHP_EOL;
+    exit;
+} 
+
+do {
+    echo "商品の価格を入力して下さい:";
+    $price = fgets(STDIN);
+
+    if (isset($rem)) {
+        $rem = $rem - $price;
+
+    } else {
+        $rem = ($money - $price);
+    }
 
     if ($rem < 0){
+        $shopping_flag = "false";
         break;
     }
 
-    echo "商品の価格を入力して下さい:";
-    $price2 = fgets(STDIN);
-    $price = $price + $price2;
-}
+    echo "残高は{$rem}円です。" . PHP_EOL;
 
-echo <<< EOM
-        チャージ金額を上回るため購入できません。
-        買い物を終了します。
-EOM;
+} while ($rem >= 0);
+
+if ($shopping_flag == "false"){
+    echo "チャージ金額を上回るため購入できません。" . PHP_EOL . "買い物を終了します。" . PHP_EOL;
+    exit;
+} 
